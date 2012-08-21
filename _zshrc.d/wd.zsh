9aaf46ff1210b0d674427f9a1af930e695bb8cdc
Evan Culver <evan@evanculver.com>
2012-08-21 15:12:22 -0700

    Adding goie and noie zsh functions to WD config.

--- _zshrc.d/wd.zsh
+++ _zshrc.d/wd.zsh
@@ -19,3 +19,25 @@ alias update_db_widget='cp /Volumes/eculver/projects/wd-app/resources/widget/dra
 # Functions
 # ------------------------------------------------------------------------
 
+goie () {
+    CONF=/Users/evan/projects/wd-legacy/app/config/config.php
+    EXT=.bak
+
+    if [[ ! -e ${CONF}${EXT} ]] then
+        sed -i $EXT -e "s/'is\-build' => false/'is-build' => true/g" $CONF
+    else
+        echo "already in IE mode?\n"
+    fi
+}
+
+noie () {
+    CONF=/Users/evan/projects/wd-legacy/app/config/config.php
+    EXT=.bak
+
+    if [[ -e ${CONF}${EXT} ]] then
+        mv ${CONF}{${EXT},}
+    else
+        echo "not in IE mode?\n"
+    fi
+}
+
