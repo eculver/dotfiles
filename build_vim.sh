e10680d9daa6086a1bef35efd4ee000b874eb3ca
Evan Culver <eculver@uber.com>
2016-10-30 14:48:34 -0700

    vim: update build script to accommodate for Ruby runtime issues w/ 7.4.x on MacOS

--- build_vim.sh
+++ build_vim.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
-
+#
 # Steps for building vim w/ all the features I like
+#
+# ----------------------------------------------------------------------------
+# NOTE(eculver-20161030): Latest versions of vim (7.4.x) don't play nicely
+# with the Ruby runtime on MacOS. Specifically, any Ruby command results in a
+# segv (segmentation fault). I'm not sure why, what *does* work is using Brew:
+#
+# 0. Switch to system Ruby first:
+# rbenv local system
+#
+# 1. Use homebrew with the options we want (use --HEAD for latest) 
+# brew install vim --with-lua --with-custom-ruby --with-python3 --with-tcl [--HEAD]
+# ----------------------------------------------------------------------------
 
 # 0. Install dependencies
 # brew/apt-get install lua
@@ -13,15 +25,18 @@ else
     cd $VIM_HOME && hg update
 fi
 
-# 2. Configure
+# 2. Make sure build env is "clean"
+make distclean
+
+# 3. Configure
 ./configure --prefix=/usr/local --with-features=huge --enable-fail-if-missing \
     --enable-cscope --enable-multibyte \
     --enable-rubyinterp --enable-perlinterp \
     --enable-pythoninterp --with-python-config-dir=/usr/lib/python2.7/config \
     --enable-luainterp=dynamic --with-lua-prefix=/usr/local
 
-# 3. Build
+# 4. Build
 make clean && make
 
-# 4. install
+# 3. install
 sudo make install
