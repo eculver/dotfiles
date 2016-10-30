fafbe35f0f4534bf4358621a7da1d72e3258a1e5
Evan Culver <eculver@uber.com>
2016-10-30 14:51:52 -0700

    zshrc: add common formatting escape helpers

--- _zshrc.d/common.zsh
+++ _zshrc.d/common.zsh
@@ -16,6 +16,21 @@ bindkey '^R' history-incremental-search-backward
 
 
 # ------------------------------------------------------------------------
+# Formatting Escape Sequences
+# ------------------------------------------------------------------------
+
+autoload colors
+if [[ "$terminfo[colors]" -gt 8 ]]; then
+    colors
+fi
+for COLOR in RED GREEN YELLOW BLUE MAGENTA CYAN BLACK WHITE; do
+    eval $COLOR='$fg_no_bold[${(L)COLOR}]'
+    eval BOLD_$COLOR='$fg_bold[${(L)COLOR}]'
+done
+eval RESET='$reset_color'
+
+
+# ------------------------------------------------------------------------
 # Aliases
 # ------------------------------------------------------------------------
 
