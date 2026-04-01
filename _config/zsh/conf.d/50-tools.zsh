ecf81d1ab7ae751ed6adfe9e3afdda36793b0f4a
Evan Culver <e@eculver.io>
2026-04-01 08:49:32 -0700

    zsh: bun and pnpm sourcing blocks

--- _config/zsh/conf.d/50-tools.zsh
+++ _config/zsh/conf.d/50-tools.zsh
@@ -6,3 +6,26 @@
 
 # From external packages (Homebrew, etc.)
 [[ -d /usr/local/share/zsh/site-functions ]] && source /usr/local/share/zsh/site-functions
+
+# ------------------------------------------------------------------------
+# bun
+# ------------------------------------------------------------------------
+
+[ -s "$HOME/.bun/_bun" ] && source "$HOME/.bun/_bun"
+. "$HOME/.local/bin/env"
+
+# ------------------------------------------------------------------------
+# pnpm
+# ------------------------------------------------------------------------
+
+export PNPM_HOME="$HOME/.local/share/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+
+# ------------------------------------------------------------------------
+# OrbStack
+# ------------------------------------------------------------------------
+
+source ~/.orbstack/shell/init.zsh 2>/dev/null || :
